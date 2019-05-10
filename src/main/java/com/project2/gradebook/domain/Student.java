b5a0d26d5dd9a40e5e39f0579cb000e725b00148
Prajakta6567 <prajaktabakshi0800@gmail.com>
2019-05-09 21:39:46 -0400

    initial commit

---
+++ src/main/java/com/project2/gradebook/domain/Student.java
@@ -0,0 +1,33 @@
+package com.project2.gradebook.domain;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+	@JacksonXmlRootElement(localName="student")
+	public class Student {
+		private String name; 
+		private String grade;
+		
+		public Student() {		
+		}
+		
+		public Student(String name, String grade) {
+			this.name = name;
+			this.grade = grade;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getGrade() {
+			return grade;
+		}
+		
+		public void setGrade(String grade) {
+			this.grade = grade;
+		} 		
+}
